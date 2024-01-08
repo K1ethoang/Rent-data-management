2a1644936615a917c0a8480524b739dcf04e93f1
Kiet Hoang <kiethoang101@gmail.com>
2024-01-08 23:43:53 +0700

    Fix: validator date of contract

--- src/main/java/com/example/demo/utils/MyUtils.java
+++ src/main/java/com/example/demo/utils/MyUtils.java
@@ -3,15 +3,13 @@ package com.example.demo.utils;
 import lombok.extern.log4j.Log4j2;
 
 import java.text.NumberFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Log4j2
 public final class MyUtils {
-    private static final String DATE_FORMAT = "yyyy/MM/dd";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     public static double stringToDouble(String s) {
         try {
@@ -29,38 +27,17 @@ public final class MyUtils {
         }
     }
 
-//    public static boolean isNull(String s) {
-//        return s == null;
-//    }
-
-//    public static boolean isEmpty(String s) {
-//        return s.isEmpty();
-//    }
-
     public static String formatMoney(String money) {
         NumberFormat formatter = NumberFormat.getNumberInstance();
         return formatter.format(stringToDouble(money));
     }
 
     public static LocalDate stringToDate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        return LocalDate.parse(date, formatter);
-    }
-
-
-    public static boolean isValidDate(String value) {
-        Date date;
-
         try {
-            SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
-            date = formatter.parse(value);
-
-            if (!value.equals(formatter.format(date))) {
-                return false;
-            }
-        } catch (ParseException e) {
-            return false;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+            return LocalDate.parse(date, formatter);
+        } catch (DateTimeException e) {
+            return null;
         }
-        return true;
     }
 }
--- src/main/java/com/example/demo/utils/validator/ContractValidator.java
+++ src/main/java/com/example/demo/utils/validator/ContractValidator.java
@@ -9,41 +9,42 @@ import com.example.demo.utils.MyUtils;
 import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Log4j2
 public class ContractValidator {
-    private static void notNullStartDate(String startDate) throws NotNullException {
+    public static void notNullStartDate(String startDate) throws NotNullException {
         if (startDate == null || startDate.trim().isEmpty())
             throw new NotNullException(ContractMessage.NOT_NULL_START_DATE);
     }
 
-    private static void notNullEndDate(String endDate) throws NotNullException {
-        if (endDate == null || endDate.trim().isEmpty()) throw new NotNullException(ContractMessage.NOT_NULL_END_DATE);
+    public static void notNullEndDate(String endDate) throws NotNullException {
+        if (endDate == null || endDate.trim().isEmpty())
+            throw new NotNullException(ContractMessage.NOT_NULL_END_DATE);
     }
 
-    private static void notNullCustomerId(String customerId) throws NotNullException {
+    public static void notNullCustomerId(String customerId) throws NotNullException {
         if (customerId == null || customerId.trim().isEmpty())
             throw new NotNullException(ContractMessage.NOT_NULL_CUSTOMER_ID);
     }
 
-    private static void notNullApartmentId(String apartmentId) throws NotNullException {
+    public static void notNullApartmentId(String apartmentId) throws NotNullException {
         if (apartmentId == null || apartmentId.trim().isEmpty())
             throw new NotNullException(ContractMessage.NOT_NULL_APARTMENT_ID);
     }
 
-    private static void invalidStartDate(String startDate) throws InValidException {
-        if (!MyUtils.isValidDate(startDate)) throw new InValidException(ContractMessage.INVALID_START_DATE);
+    public static void invalidStartDate(String startDate) throws InValidException {
+        if (MyUtils.stringToDate(startDate.trim()) == null)
+            throw new InValidException(ContractMessage.INVALID_START_DATE);
     }
 
-    private static void invalidEndDate(String endDate) throws InValidException {
-        if (!MyUtils.isValidDate(endDate)) throw new InValidException(ContractMessage.INVALID_END_DATE);
+    public static void invalidEndDate(String endDate) throws InValidException {
+        if (MyUtils.stringToDate(endDate.trim()) == null)
+            throw new InValidException(ContractMessage.INVALID_END_DATE);
     }
 
-    public static void invalidStartDateAndEndDate(String startDate, String endDate) throws InValidException {
-        LocalDate startD = MyUtils.stringToDate(startDate);
-        LocalDate endD = MyUtils.stringToDate(endDate);
-
-        if (startD.isAfter(endD) || startD.isEqual(endD))
+    public static void invalidStartDateAndEndDate(LocalDate startDate, LocalDate endDate) throws InValidException {
+        if (startDate.isAfter(endDate) || startDate.isEqual(endDate))
             throw new InValidException(ContractMessage.START_DATE_LESS_THAN_END_DATE);
     }
 
@@ -52,32 +53,41 @@ public class ContractValidator {
         invalidStartDate(contractDTO.getStartDate());
         notNullEndDate(contractDTO.getEndDate());
         invalidEndDate(contractDTO.getEndDate());
-        invalidStartDateAndEndDate(contractDTO.getStartDate(), contractDTO.getEndDate());
+        invalidStartDateAndEndDate(Objects.requireNonNull(MyUtils.stringToDate(contractDTO.getStartDate())), Objects.requireNonNull(MyUtils.stringToDate(contractDTO.getEndDate())));
         notNullCustomerId(contractDTO.getCustomerId());
         notNullApartmentId(contractDTO.getApartmentId());
+
+        contractDTO.setStartDate(contractDTO.getStartDate().trim());
+        contractDTO.setEndDate(contractDTO.getEndDate().trim());
+        contractDTO.setApartmentId(contractDTO.getApartmentId().trim());
+        contractDTO.setCustomerId(contractDTO.getCustomerId().trim());
     }
 
     public static void validatorContactUpdateDTO(ContractUpdateDTO contractUpdateDTO) {
         if (contractUpdateDTO.getStartDate() != null) {
             notNullStartDate(contractUpdateDTO.getStartDate());
             invalidStartDate(contractUpdateDTO.getStartDate());
+            contractUpdateDTO.setStartDate(contractUpdateDTO.getStartDate().trim());
         }
 
         if (contractUpdateDTO.getEndDate() != null) {
             notNullEndDate(contractUpdateDTO.getEndDate());
             invalidEndDate(contractUpdateDTO.getEndDate());
+            contractUpdateDTO.setEndDate(contractUpdateDTO.getEndDate().trim());
         }
 
         if (contractUpdateDTO.getStartDate() != null && contractUpdateDTO.getEndDate() != null) {
-            invalidStartDateAndEndDate(contractUpdateDTO.getStartDate(), contractUpdateDTO.getEndDate());
+            invalidStartDateAndEndDate(Objects.requireNonNull(MyUtils.stringToDate(contractUpdateDTO.getStartDate())), Objects.requireNonNull(MyUtils.stringToDate(contractUpdateDTO.getEndDate())));
         }
 
         if (contractUpdateDTO.getCustomerId() != null) {
             notNullCustomerId(contractUpdateDTO.getCustomerId());
+            contractUpdateDTO.setCustomerId(contractUpdateDTO.getCustomerId().trim());
         }
 
         if (contractUpdateDTO.getApartmentId() != null) {
             notNullApartmentId(contractUpdateDTO.getApartmentId());
+            contractUpdateDTO.setApartmentId(contractUpdateDTO.getApartmentId().trim());
         }
     }
 
