f29ea75253acb6afa024e7856b8f8ce60481b470
Kiet Hoang <kiethoang101@gmail.com>
2024-03-02 13:03:46 +0700

    Add: set up API Doc (swagger)

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+//@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build();
+    }
+}
